37c2ad6b236a58dedc346b896fff97f379c93220
Javier Quiroga <javierquirogae@gmail.com>
2022-09-14 21:15:06 -0400

    calculator logic

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,26 +21,40 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
-  document.getElementById("loan-amount").placeholder = 500000;
-  document.getElementById("loan-years").placeholder = 30;
-  document.getElementById("loan-rate").placeholder = 2;
+  let initialValues = [600000,30,6];
+  document.getElementById("loan-amount").placeholder = initialValues[0];
+  document.getElementById("loan-years").placeholder = initialValues[1];
+  document.getElementById("loan-rate").placeholder = initialValues[2];
+  calculateMonthlyPayment(initialValues);
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-
+  let valuesObj = getCurrentUIValues();
+  values = Object.values(valuesObj);
+  document.getElementById("loan-amount").placeholder = values[0];
+  document.getElementById("loan-years").placeholder = values[1];
+  document.getElementById("loan-rate").placeholder = values[2];
+  calculateMonthlyPayment(values);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-
+  console.log(values)
+  let i = values[2]/1200;
+  let n = values[1]*12;
+  let numerator = values[0]*i;
+  console.log(numerator);
+  let denominator = 1 - Math.pow(1 + i, -n);
+  console.log(denominator);
+  updateMonthly(numerator/denominator)
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
-
+  console.log(monthly);
 }
